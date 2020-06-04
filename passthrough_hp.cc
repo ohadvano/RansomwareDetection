361d6477e869558baa7f9e6934a5f9fc45789423
ohadvano <ohadvano@gmail.com>
2020-06-04 20:27:51 +0300

    compiling

--- passthrough_hp.cc
+++ passthrough_hp.cc
@@ -1686,6 +1686,7 @@ static void maximize_fd_limit()
 
 int main(int argc, char *argv[]) 
 {
+printf("1\n");
     // Parse command line options
     auto options {parse_options(argc, argv)};
 
@@ -1694,11 +1695,13 @@ int main(int argc, char *argv[])
     // so try to get rid of any resource softlimit.
     maximize_fd_limit();
 
+printf("2\n");
+
     // Initialize filesystem root
     fs.root.fd = -1;
     fs.root.nlookup = 9999;
     fs.timeout = options.count("nocache") ? 0 : 86400.0;
-
+printf("3\n");
     struct stat stat;
     auto ret = lstat(fs.source.c_str(), &stat);
     if (ret == -1)
@@ -1706,11 +1709,11 @@ int main(int argc, char *argv[])
     if (!S_ISDIR(stat.st_mode))
         errx(1, "ERROR: source is not a directory");
     fs.src_dev = stat.st_dev;
-
+printf("4\n");
     fs.root.fd = open(fs.source.c_str(), O_PATH);
     if (fs.root.fd == -1)
         err(1, "ERROR: open(\"%s\", O_PATH)", fs.source.c_str());
-
+printf("5\n");
     // Initialize fuse
     fuse_args args = FUSE_ARGS_INIT(0, nullptr);
     if (fuse_opt_add_arg(&args, argv[0]) ||
@@ -1718,6 +1721,8 @@ int main(int argc, char *argv[])
         fuse_opt_add_arg(&args, "default_permissions,fsname=hpps") ||
         (options.count("debug-fuse") && fuse_opt_add_arg(&args, "-odebug")))
         errx(3, "ERROR: Out of memory");
+
+printf("6\n");
 
     fuse_lowlevel_ops sfs_oper {};
     assign_operations(sfs_oper);
@@ -1727,7 +1732,7 @@ int main(int argc, char *argv[])
 
     if (fuse_set_signal_handlers(se) != 0)
         goto err_out2;
-
+printf("7\n");
     // Don't apply umask, use modes exactly as specified
     umask(0);
 
