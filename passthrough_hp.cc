b41244be17d4d3d665c6cae80b15cad94380d8e9
ohadvano <ohadvano@gmail.com>
2020-06-05 11:52:27 +0300

    test

--- passthrough_hp.cc
+++ passthrough_hp.cc
@@ -279,19 +279,25 @@ static void sfs_init(void *userdata, fuse_conn_info *conn)
 
 static void sfs_getattr(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) 
 {
-    // // Ransomware monitor
-    // pid_t callingPid = getpid();
-    // FsAction action = GetAttrAction(
-    //     ino, 
-    //     FileInfoContract(fi->flags, fi->writepage, fi->direct_io, fi->keep_cache, fi->flush, fi->nonseekable, fi->flock_release, 
-    //                         fi->cache_readdir, fi->padding, fi->padding2, fi->fh, fi->lock_owner, fi->poll_events), 
-    //     callingPid);
+printf("1");
+    // Ransomware monitor
+    pid_t callingPid = getpid();
+printf("2");
+    FsAction action = GetAttrAction(
+        ino, 
+        FileInfoContract(fi->flags, fi->writepage, fi->direct_io, fi->keep_cache, fi->flush, fi->nonseekable, fi->flock_release, 
+                            fi->cache_readdir, fi->padding, fi->padding2, fi->fh, fi->lock_owner, fi->poll_events), 
+        callingPid);
 
-    // bool shouldIgnoreRequest = PerformRansomwareValidations(action) == false;
-    // if (shouldIgnoreRequest)
-    // {
-    //     return;
-    // }
+printf("3");
+
+    bool shouldIgnoreRequest = PerformRansomwareValidations(action) == false;
+    if (shouldIgnoreRequest)
+    {
+        return;
+    }
+
+printf("4");
 
     (void)fi;
     Inode& inode = get_inode(ino);
