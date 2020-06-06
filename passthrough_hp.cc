6f06de2053c88eea62f0943f8e3d1dfb5006d9de
ohadvano <ohadvano@gmail.com>
2020-06-06 11:47:21 +0300

    test

--- passthrough_hp.cc
+++ passthrough_hp.cc
@@ -281,22 +281,19 @@ static void sfs_getattr(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi)
 {
 printf("1");
     // Ransomware monitor
-    // pid_t callingPid = getpid();
+    pid_t callingPid = getpid();
 printf("2");
     // FsAction action = GetAttrAction(
     //     ino, 
     //     FileInfoContract(fi->flags, fi->writepage, fi->direct_io, fi->keep_cache, fi->flush, fi->nonseekable, fi->flock_release, 
     //                         fi->cache_readdir, fi->padding, fi->padding2, fi->fh, fi->lock_owner, fi->poll_events), 
     //     callingPid);
-
 printf("3");
-
     // bool shouldIgnoreRequest = PerformRansomwareValidations(action) == false;
     // if (shouldIgnoreRequest)
     // {
     //     return;
     // }
-
 printf("4");
 
     (void)fi;
