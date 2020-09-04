112f0fdbd5d2efbdfa534191cf8a5f033d5b1689
ohadvano <ohadvano@gmail.com>
2020-09-04 16:25:34 +0300

    .

--- Testings/History.cpp
+++ Testings/History.cpp
@@ -29,6 +29,29 @@ namespace ActionsHistory
             R _action;
             time_t _actionTime;
     };
+
+    template <typename T>
+    class ActionHistory
+    {
+        public:
+            ActionHistory()
+            {
+                _actionRecords = new list<ActionRecord<T>>();
+            }
+
+            void AddNewAction<T>(T newAction)
+            {
+                _actionRecords->push_front(ActionRecord(newAction));
+            }
+
+            ~ActionsHistory()
+            {
+                delete _actionRecords;
+            }
+            
+        private:
+            std::list<ActionRecord<T>>* _actionRecords;
+    };
 }
 
 #endif
