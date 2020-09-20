1d4c4cc6d4d80f88b43d3ff68ba96b3eaa0a985e
ohadvano <ohadvano@gmail.com>
2020-09-20 18:08:48 +0300

    .

--- RwMonitor/TempWriter.cpp
+++ RwMonitor/TempWriter.cpp
@@ -22,7 +22,7 @@ namespace TempFile
 
             TempWriter(char* tempFilePath)
             {
-                string path = "Run//";
+                string path = "//home//ohadoz//Desktop//RansomwareDetection//Run//";
                 path += tempFilePath;
 
                 char* finalTempFilePath = (char*)malloc(path.length() + 1);
