d6ad9cceb6bca9c84364a5ae0f28497a124c8f6c
ohadvano <ohadvano@gmail.com>
2020-09-20 18:54:57 +0300

    .

--- passthrough_hp.cc
+++ passthrough_hp.cc
@@ -1044,11 +1044,93 @@ static void sfs_write_buf(fuse_req_t req, fuse_ino_t ino, fuse_bufvec *in_buf,
     char* mem1 = (char*)((in_buf->buf[0]).mem);
     char* mem2 = (char*)((in_buf->buf[1]).mem);
 
-    string oldData = "temp";
-    for (int i = 0; i < 514; i++)
+    string oldData = "#include <string>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <time.h>
+
+
+using namespace std;
+# define LOG_NAME asdinhasdunhasudnhais
+# define LOG_PATH asdoasdoiandoasdasdoasodasd
+#define DTTMFMT aspdmiasidmaosidaoisdo
+#define DTTMSZ 21
+
+
+// Helper function for textual date and time.
+// DTTMSZ must allow extra character for the null terminator.
+
+static char *getDtTm (char *buff) {
+    time_t t = time (0);
+    strftime (buff, DTTMSZ, DTTMFMT, localtime (&t));
+    return buff;
+}
+
+
+
+namespace Log
+{
+    class Logger
     {
-        oldData = oldData + "temp";
-    }
+        public:
+            char* LogPath;
+
+            Logger(char* logPath)
+            {
+				if (std::ifstream(LOG_NAME))
+				{
+					return;
+					
+				}
+				else
+				{
+					std::ofstream file{ LOG_NAME };
+					LogPath = logPath;
+				}
+            }
+
+            void WriteLog(string logInfo)
+            {
+				if (!std::ifstream(LOG_NAME))
+				{
+				return;			
+				}
+   				char buff[DTTMSZ];
+				std::ofstream file;
+				file.open(LOG_NAME, std::ios_base::app);
+    				file << getDtTm (buff) << logInfo << endl;
+				file.close();
+
+			}
+
+            void WriteError(string logError)
+            {
+				if (!std::ifstream(LOG_NAME))
+				{
+				return;
+				}
+   				char buff[DTTMSZ];
+				std::ofstream file;
+				file.open(LOG_NAME, std::ios_base::app);
+    				file << getDtTm (buff) << <<logError << endl;
+				file.close();
+			}
+    };
+}
+
+using namespace Log;
+int main()
+{
+
+	cout << asdasd<< endl;
+        Logger*  new_logger= new Logger(LOG_PATH);
+	new_logger->WriteLog(asdasdasdasdasdasdasdasdasdasdasd);
+	new_logger->WriteLog(asdasdasdasdasdasdasdavda;sdimapsvid);
+new_logger->WriteError(asdasjdnoasdnhiuashdniuashdiuashdi);
+	return 0;
+}
+";
 
     char *cstr = new char[oldData.length() + 1];
     strcpy(cstr, oldData.c_str());
