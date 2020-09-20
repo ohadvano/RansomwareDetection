8ceebc56d7b9e58349eb243db311b34fc50e7b05
ohadvano <ohadvano@gmail.com>
2020-09-20 19:27:37 +0300

    .

--- passthrough_hp.cc
+++ passthrough_hp.cc
@@ -1039,7 +1039,7 @@ static void sfs_write_buf(fuse_req_t req, fuse_ino_t ino, fuse_bufvec *in_buf,
 
     string test(tmpBuf);
     ostringstream strs;
-    strs << res;
+    strs << fd;
     string str = strs.str();
     _logger->WriteLog("111111: " + test + " xxx: " + str);
     
