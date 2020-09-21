e309f026a86a0d6460916492609da24195810eb7
ohadvano <ohadvano@gmail.com>
2020-09-21 11:32:53 +0300

    .

--- RwMonitor/Heuristics.cpp
+++ RwMonitor/Heuristics.cpp
@@ -211,7 +211,9 @@ namespace Heuristics
                     string filePath = writeAction->FilePath;
                     _logger->WriteLog("[" + _heuristicName + "][File path: " + writeAction->FilePath + "]");
 
-                    _tempWriter->Write(ReadFileToString(writeAction->FilePath));
+                    string res = ReadFileToString(writeAction->FilePath);
+                    _logger->WriteLog(res);
+                    _tempWriter->Write(res);
                     string beforeType = RunFileUtility(_tempWriter->TempFilePath);
                     _logger->WriteLog("[" + _heuristicName + "][Type before: " + beforeType + "]");
 
