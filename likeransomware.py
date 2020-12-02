d0069e79964d55718eaa9f6176f324aaf5a99ae6
ohadvano <ohadvano@gmail.com>
2020-12-02 18:43:59 +0200

    .

---
+++ likeransomware.py
@@ -0,0 +1,10 @@
+import random
+import string
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
+import threading
+import os
+import sys
+
+print("asdasdasd")
