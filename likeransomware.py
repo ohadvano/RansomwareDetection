e6b1b95d6423327297f88ac507282ffa4404fc98
ohadvano <ohadvano@gmail.com>
2020-12-13 14:31:36 +0200

    .

--- likeransomware.py
+++ likeransomware.py
@@ -25,12 +25,22 @@ def encrypt_all_files():
                     pass
         break
 
-def GetAllFiles(base_path):
-    f = []
-    for (dirpath, dirnames, filenames) in walk(base_path):
-        f.extend(filenames)
-        break
-    return f
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
 
 ###################### main ######################
 
