305312f0b974037dd1de6f28af7b14af3ea36935
ohadvano <ohadvano@gmail.com>
2020-12-13 14:38:36 +0200

    .

--- likeransomware.py
+++ likeransomware.py
@@ -8,21 +8,12 @@ import os
 import sys
 from os import walk
 
-def encrypt_all_files():
-    password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32)) 
-    for root, dirs, files in os.walk("/TestFiles", topdown=True):  # iterate from home
-        for filename in files:
-            if filename.endswith(ext) and not filename.startswith(encrypted_symbol):  
-                try:
-                    key = SHA256.new(password.encode('utf-8')).digest()  
-                    filepath = os.path.abspath(os.path.join(root, filename))
-                    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                    # encrypt_file(key, filename, filepath)  # calls the encrypt function
-                    # os.remove(filepath)  # deletes the file
-                    break
-                except:
-                    pass
-        break
+def EncryptFile(fileToEncrypt):
+    print(file_to_encrypt)
+
+def EncryptAllFiles(filesToEncrypt):
+    for file_to_encrypt in filesToEncrypt:
+        EncryptFile(file_to_encrypt)
 
 def GetAllFiles(dirName):
     ext = ('.jpg', '.png', '.bmp', '.raw', '.c', '.java', '.class', '.cpp', '.h', '.jar', '.txt', '.doc', '.docx', '.pdf', '.ptx', '.ppt', '.rar', '.zip', '.7z', '.mp3', '.mp4', '.mpg', '.mpeg', '.avi', '.tar.gz', '.sql', '.xml', '.py', '.js', '.php', '.pps', '.cs', '.xls', '.xlsx', '.3gp', '.mov', '.mkv', '.vob', '.wps', '.odt')
@@ -41,6 +32,4 @@ def GetAllFiles(dirName):
 
 base_path = "/home/ohadoz/Desktop/RansomwareDetection/TestData/output/TestFiles"
 files_to_encrypt = GetAllFiles(base_path)
-for file in files_to_encrypt:
-    print(file)
-    print("\n")
+EncryptAllFiles(files_to_encrypt)
